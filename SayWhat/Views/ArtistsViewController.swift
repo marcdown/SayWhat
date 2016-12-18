8525d4a84256372a6d3d8bda797b7a4d533812a0
Bereket <bereket6725@gmail.com>
2016-12-18 16:29:27 -0500

    removed older commented out code

--- SayWhat/Views/ArtistsViewController.swift
+++ SayWhat/Views/ArtistsViewController.swift
@@ -68,55 +68,6 @@ class ArtistsViewController: UIViewController, UISearchBarDelegate, UITableViewD
         }
     }
     
-//    func search(artist: String?) {
-//       
-//        guard let artist = artist else {
-//            return
-//        }
-//        
-//        //makeAPICall(artist: String)
-//        reset()
-//       //1
-//        let urlEncodedArtist = artist.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-//        let url: URL = URL(string: "https://api.spotify.com/v1/search?q=\(urlEncodedArtist)&type=artist")!
-//        let session = URLSession.shared
-//        
-//        
-//        //2
-//        let task = session.dataTask(with: url) { (data, response, error) in
-//            guard let data = data else {
-//                return
-//            }
-//        
-//        
-//        //3
-//            do {
-//                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject]
-//                
-//                if let artists = json?["artists"]?["items"] as? [AnyObject] {
-//                    if artists.isEmpty {
-//                        self.displayErrorAlert(message: .NoResults)
-//                    } else {
-//                        for artist in artists {
-//                            let artistModel = ArtistModel(artist: artist as! [String : AnyObject])
-//                            self.searchResults.append(artistModel)
-//                        }
-//                        
-//                        
-//                        OperationQueue.main.addOperation {
-//                            self.tableView.reloadData()
-//                        }
-//                    }
-//                    
-//                }
-//            } catch {
-//                return
-//            }
-//        }
-//        
-//        task.resume()
-//    }
-    
     func searchForArtist(artist: String?){
         guard let artist = artist else {
             return
@@ -145,7 +96,6 @@ class ArtistsViewController: UIViewController, UISearchBarDelegate, UITableViewD
             }
             self.serializeAndParseJSON(data: data!) { artistsArray in
                 completion(artistsArray)
-                
                             }
         }
         task.resume()
